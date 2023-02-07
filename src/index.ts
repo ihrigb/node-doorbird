cdf9ddd06a8c18ac29b143838379bd981fa4568e
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2023-02-07 17:22:07 +0100

    Improve documentation

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,38 @@ import * as dgram from "dgram";
 import * as https from "https";
 import axios, { AxiosRequestConfig } from "axios";
 
+// Helper to define integer ranges as parameters
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+/**
+ * Identifier of UDP packages (as of documentation)
+ */
 const udpIdentifier = Buffer.from([0xde, 0xad, 0xbe]);
+/**
+ * Fixed argon key length (as of documentation)
+ */
 const argonKeyLength = 32;
 
+/**
+ * Scheme for API communication with the door station.
+ */
 export enum Scheme {
   http = "http",
   https = "https",
 }
 
+/**
+ * Options to initialize the Doorbird client.
+ */
 export interface DoorbirdOptions {
   scheme: Scheme;
   host: string;
@@ -19,22 +43,37 @@ export interface DoorbirdOptions {
   password: string;
 }
 
+/**
+ * Generic response wrapper of the Doorbird API.
+ */
 export interface Response<Type> {
   BHA: Type;
 }
 
+/**
+ * Basic BHA object for responses.
+ */
 export interface BaseBHA {
   RETURNCODE: string;
 }
 
+/**
+ * Specific BHA object for session responses.
+ */
 export interface SessionBHA extends BaseBHA {
   SESSIONID: string;
 }
 
+/**
+ * Specific BHA object for info responses.
+ */
 export interface DoorbirdInfoBHA {
   VERSION: DoorbirdInfoBHAVersion[];
 }
 
+/**
+ * Doorbird info object.
+ */
 export interface DoorbirdInfoBHAVersion {
   FIRMWARE: string;
   BUILD_NUMBER: string;
@@ -43,53 +82,86 @@ export interface DoorbirdInfoBHAVersion {
   "DEVICE-TYPE": string;
 }
 
+/**
+ * Type for Doorbird favorites.
+ */
 export enum FavoriteType {
   sip = "sip",
   http = "http",
 }
 
+/**
+ * Doorbird favorites.
+ */
 export type Favorites = {
   sip?: Favorite;
   http?: Favorite;
 };
 
+/**
+ * A single Doorbird favorite.
+ */
 export type Favorite = {
   [id: string]: FavoriteInfo;
 };
 
+/**
+ * Information on a Doorbird favorite.
+ */
 export interface FavoriteInfo {
   title: string;
   value: string;
 }
 
+/**
+ * A schedule is an array of ScheduleEntries.
+ */
 export type Schedule = ScheduleEntry[];
 
+/**
+ * A single entry of a schedule.
+ */
 export interface ScheduleEntry {
   input: "doorbell" | "motion" | "rfid";
   param?: string;
   output: ScheduleEntryOutput;
 }
 
+/**
+ * Output of a schedule entry.
+ */
 export interface ScheduleEntryOutput {
   event: "notify" | "sip" | "relay" | "http";
   param?: string;
   schedule: "once" | ScheduleEntrySchedule;
 }
 
+/**
+ * Specific schedule for a schedule entry.
+ */
 export interface ScheduleEntrySchedule {
   "from-to"?: FromTo[];
   weekdays?: FromTo[];
 }
 
+/**
+ * Object that defines a timespan.
+ */
 export interface FromTo {
   from: string;
   to: string;
 }
 
+/**
+ * Specific BHA for SIP status.
+ */
 export interface SipStatusBHA extends BaseBHA {
   SIP: SipStatus[];
 }
 
+/**
+ * SIP status object.
+ */
 export interface SipStatus {
   ENABLE: string;
   PRIORITIZE_APP: string;
@@ -115,20 +187,35 @@ export interface SipStatus {
   CALL_TIME_LIMIT: string;
 }
 
+/**
+ * Event object for doorbell rings.
+ */
 export interface RingEvent {
   intercomId: string;
   event: string;
   timestamp: Date;
 }
 
+/**
+ * Event object for motion detection.
+ */
 export interface MotionEvent {
   intercomId: string;
   timestamp: Date;
 }
 
+/**
+ * Callback for ring events.
+ */
 export type RingCallback = (event: RingEvent) => void;
+/**
+ * Callback for motion events.
+ */
 export type MotionCallback = (event: MotionEvent) => void;
 
+/**
+ * Wrapper class for a UDP socket, that is capable to handle Doorbird's UDP messages.
+ */
 export class DoorbirdUdpSocket {
   private username: string;
   private password: string;
@@ -138,6 +225,14 @@ export class DoorbirdUdpSocket {
   private ringListeners: RingCallback[] = [];
   private motionListeners: MotionCallback[] = [];
 
+  /**
+   * Construct a new DoorbirdUdpSocket
+   *
+   * @param port Doorbird sends to ports 6524 and 35344.
+   * @param username username of the Doorbird user.
+   * @param password password of the Doorbird user.
+   * @param suppressBurst flag to suppress multiple burst messages (callback is only called once)
+   */
   constructor(
     port: 6524 | 35344,
     username: string,
@@ -172,7 +267,6 @@ export class DoorbirdUdpSocket {
   };
 
   private onMessage = async (msg: Buffer) => {
-
     const identifier = msg.slice(0, 3);
     const version = msg.slice(3, 4);
     const opslimit = msg.slice(4, 8);
@@ -191,7 +285,7 @@ export class DoorbirdUdpSocket {
 
     if (this.suppressBurst) {
       const eventTimestamp = new Date().valueOf();
-      if ((eventTimestamp - this.lastEventTimestamp) < 1000) {
+      if (eventTimestamp - this.lastEventTimestamp < 1000) {
         return;
       }
       this.lastEventTimestamp = eventTimestamp;
@@ -233,14 +327,27 @@ export class DoorbirdUdpSocket {
     });
   };
 
+  /**
+   * Register a ring listener.
+   *
+   * @param listener ring listener
+   */
   registerRingListener(listener: RingCallback): void {
     this.ringListeners.push(listener);
   }
 
+  /**
+   * Register a motion listner.
+   *
+   * @param listener motion listener
+   */
   registerMotionListener(listener: MotionCallback): void {
     this.motionListeners.push(listener);
   }
 
+  /**
+   * Close the UDP socket.
+   */
   close(): void {
     if (this.server) {
       this.server.close();
@@ -248,13 +355,26 @@ export class DoorbirdUdpSocket {
   }
 }
 
+/**
+ * Doorbird client class.
+ */
 export default class Doorbird {
   private options: DoorbirdOptions;
 
+  /**
+   * Construct a Doorbird client.
+   *
+   * @param options options for the Doorbird client
+   */
   constructor(options: DoorbirdOptions) {
     this.options = options;
   }
 
+  /**
+   * Initialize a session.
+   *
+   * @returns session response
+   */
   async initializeSession(): Promise<Response<SessionBHA>> {
     const resp = await axios.get<Response<SessionBHA>>(
       this.uri(`/bha-api/getsession.cgi`),
@@ -263,6 +383,12 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Destroy a session.
+   *
+   * @param session session response or id
+   * @returns session response
+   */
   async destroySession(
     session: Response<SessionBHA> | string
   ): Promise<Response<SessionBHA>> {
@@ -276,6 +402,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Get info of the Doorbird installation.
+   *
+   * @returns Doorbird info response
+   */
   async getInfo(): Promise<Response<DoorbirdInfoBHA>> {
     const resp = await axios.get<Response<DoorbirdInfoBHA>>(
       this.uri(`/bha-api/info.cgi`),
@@ -295,6 +426,12 @@ export default class Doorbird {
     return this.toggleRelay(relay);
   }
 
+  /**
+   * Toggle a relay.
+   *
+   * @param relay the ID of the relay to be toggled
+   * @returns base response
+   */
   async toggleRelay(relay: string): Promise<Response<BaseBHA>> {
     const resp = await axios.get<Response<BaseBHA>>(
       this.uri(`/bha-api/open-door.cgi?r=${relay}`),
@@ -303,6 +440,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Enable the infra red lights of the door station.
+   *
+   * @returns base response
+   */
   async lightOn(): Promise<Response<BaseBHA>> {
     const resp = await axios.get<Response<BaseBHA>>(
       this.uri(`/bha-api/light-on.cgi`),
@@ -311,6 +453,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Get a list of favorites.
+   *
+   * @returns favorites list
+   */
   async listFavorites(): Promise<Favorites> {
     const resp = await axios.get<Favorites>(
       this.uri(`/bha-api/favorites.cgi`),
@@ -319,6 +466,13 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Create a new favorite.
+   *
+   * @param type new favorite type
+   * @param favoriteInfo favorite info
+   * @returns empty promise
+   */
   createFavorite(
     type: FavoriteType,
     favoriteInfo: FavoriteInfo
@@ -326,6 +480,14 @@ export default class Doorbird {
     return this.doCreateUpdateFavorite(type, favoriteInfo);
   }
 
+  /**
+   * Update a favorite.
+   *
+   * @param id id of the favorite to be updated
+   * @param type type of the favorite to be updated
+   * @param favoriteInfo new favorite info
+   * @returns empty promise
+   */
   updateFavorite(
     id: string,
     type: FavoriteType,
@@ -349,6 +511,13 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Delete a favorite.
+   *
+   * @param id id of the favorite to be deleted
+   * @param type type of the favorite to be deleted
+   * @returns empty promise
+   */
   async deleteFavorite(id: string, type: FavoriteType): Promise<void> {
     const resp = await axios.get<void>(
       this.uri(`/bha-api/favorites.cgi?action=remove&type=${type}&id=${id}`),
@@ -357,6 +526,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Get the schedule of the Doorbird system.
+   *
+   * @returns schedule response
+   */
   async getSchedule(): Promise<Schedule> {
     const resp = await axios.get<Schedule>(
       this.uri(`/bha-api/schedule.cgi`),
@@ -365,10 +539,22 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Create a schedule entry.
+   *
+   * @param scheduleEntry new schedule entry
+   * @returns empty promise
+   */
   createScheduleEntry(scheduleEntry: ScheduleEntry): Promise<void> {
     return this.updateScheduleEntry(scheduleEntry);
   }
 
+  /**
+   * Update a schedule entry.
+   *
+   * @param scheduleEntry updated schedule entry
+   * @returns empty promise
+   */
   async updateScheduleEntry(scheduleEntry: ScheduleEntry): Promise<void> {
     const resp = await axios.post<void>(
       this.uri(`/bha-api/schedule.cgi`),
@@ -377,6 +563,13 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Delete a schedule entry.
+   *
+   * @param input input type of the entry to be deleted
+   * @param param param of the entry to be deleted
+   * @returns empty promise
+   */
   async deleteScheduleEntry(
     input: "doorbell" | "motion" | "rfid",
     param: string | null
@@ -389,6 +582,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Restart the door station.
+   *
+   * @returns empty promise
+   */
   async restart(): Promise<void> {
     const resp = await axios.get<void>(
       this.uri(`/bha-api/restart.cgi`),
@@ -397,6 +595,14 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Create a SIP registration.
+   *
+   * @param user user for the sip registration
+   * @param password password for the sip registration
+   * @param url url for the sip registration
+   * @returns empty promise
+   */
   async sipRegistration(
     user: string,
     password: string,
@@ -411,6 +617,12 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Initiate a SIP call.
+   *
+   * @param url url for the SIP call
+   * @returns empty promise
+   */
   async sipCall(url: string): Promise<void> {
     const resp = await axios.get<void>(
       this.uri(`/bha-api/sip.cgi?action=makecall&url=${url}`),
@@ -419,6 +631,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * End a SIP call.
+   *
+   * @returns empty promise
+   */
   async sipHangup(): Promise<void> {
     const resp = await axios.get<void>(
       this.uri(`/bha-api/sip.cgi?action=hangup`),
@@ -427,15 +644,30 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Update SIP settings.
+   *
+   * @param enable enable or disable SIP registration after device reboot
+   * @param mic_volume microphone volume (1-100)
+   * @param spk_volume speaker volume (1-100)
+   * @param dtmf enable or disable DTMF support
+   * @param relay1_passcode pincode for triggering the door open relay
+   * @param incoming_call_enable enable or disable incoming calls
+   * @param incoming_call_user Allowed SIP user which will be authenticated for Doorbird
+   * @param anc enable or disable acoustic noise cancellation
+   * @returns empty promise
+   */
   async sipSettings(
     enable: 0 | 1,
-    mic_volume: number,
-    spk_volume: number,
+    mic_volume: IntRange<1, 100>,
+    spk_volume: IntRange<1, 100>,
     dtmf: 0 | 1,
     relay1_passcode: number,
     incoming_call_enable: 0 | 1,
     incoming_call_user: string,
-    anc: 0 | 1
+    anc: 0 | 1,
+    ringTimeLimit?: IntRange<10, 300>,
+    callTimeLimit?: IntRange<30, 300>
   ): Promise<void> {
     let url = `/bha-api/sip.cgi?action=settings`;
     if (enable) {
@@ -462,10 +694,21 @@ export default class Doorbird {
     if (anc) {
       url += `&anc=${anc}`;
     }
+    if (ringTimeLimit) {
+      url += `&ring_time_limit=${ringTimeLimit}`;
+    }
+    if (callTimeLimit) {
+      url += `&call_time_limit=${callTimeLimit}`;
+    }
     const resp = await axios.get<void>(this.uri(url), this.requestConfig());
     return resp.data;
   }
 
+  /**
+   * Get the SIP status.
+   *
+   * @returns Specific BHA for SIP status
+   */
   async sipStatus(): Promise<Response<SipStatusBHA>> {
     const resp = await axios.get<Response<SipStatusBHA>>(
       this.uri(`/bha-api/sip.cgi?action=status`),
@@ -474,6 +717,11 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Reset the SIP settings.
+   *
+   * @returns empty promise
+   */
   async sipSettingsReset(): Promise<void> {
     const resp = await axios.get<void>(
       this.uri(`/bha-api/sip.cgi?action=reset`),
@@ -482,6 +730,13 @@ export default class Doorbird {
     return resp.data;
   }
 
+  /**
+   * Start a UDP socket.
+   *
+   * @param port port to listen on
+   * @param suppressBurst suppress multiple UDP messages into a single callback
+   * @returns DoorbirdUdpSocket object
+   */
   startUdpSocket(port: 6524 | 35344, suppressBurst = false): DoorbirdUdpSocket {
     return new DoorbirdUdpSocket(
       port,
@@ -491,6 +746,11 @@ export default class Doorbird {
     );
   }
 
+  /**
+   * Get the Doorbird image url.
+   *
+   * @returns image url
+   */
   getImageUrl(): string {
     return (
       `${this.options.scheme}://${this.options.host}/bha-api/image.cgi` +
@@ -498,13 +758,23 @@ export default class Doorbird {
     );
   }
 
+  /**
+   * Get the current image.
+   *
+   * @returns buffer with image data
+   */
   async getImage(): Promise<Buffer> {
     const resp = await axios.get(this.getImageUrl(), {
-      responseType: 'arraybuffer'
+      responseType: "arraybuffer",
     });
-    return Buffer.from(resp.data, 'binary');
+    return Buffer.from(resp.data, "binary");
   }
 
+  /**
+   * Get the Doorbird video url.
+   * 
+   * @returns video url
+   */
   getVideoUrl(): string {
     return (
       `${this.options.scheme}://${this.options.host}/bha-api/video.cgi` +
@@ -521,7 +791,7 @@ export default class Doorbird {
 
     if (Scheme.https === this.options.scheme) {
       const httpsAgent = new https.Agent({
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
       });
       requestConfig.httpsAgent = httpsAgent;
     }
